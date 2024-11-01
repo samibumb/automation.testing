eeb0e65adef65e3ccabd37f0d1dc1b1a34511111
samibumb <bmb.sami@yahoo.com>
2024-11-01 13:08:56 +0200

    commit

---
+++ cypress/e2e/testing.cy.js
@@ -0,0 +1,7 @@
+describe('empty spec', () => {
+    it('passes', () => {
+      cy.visit('https://www.emag.ro');
+    })     
+})
+
+  
--- cypress/e2e/visitGoogle.cy.js
+++
@@ -1,9 +0,0 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://www.google.ro/')
-    cy.get('#L2AGLb > .QS5gu').click()
-    cy.get('.gLFyf').type('Speranta si prietenii colinde')
-    cy.get(':nth-child(1) > .eIPGRd').click()
-
-  })
-})
